2086b0362ea7daa2073415576c6745a6784ab582
Zixuan(Garrick) Zhang <garrickz2018@gmail.com>
2022-10-26 12:40:19 -0400

    add feat to goto login or signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,8 @@
 class UserController < ApplicationController
-  def login
+  def index
+    @login_type = params[:type]
+    if @login_type == nil || @login_type == ''
+      @login_type = 'login'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'user/login'
+  get 'user/index'
 
   get 'main/dashboard'
 
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  get "user/login" => 'user#login'
+  get "user/index/:type" => 'user#index'
 
   get "main/dashboard" => "main#dashboard"
 
