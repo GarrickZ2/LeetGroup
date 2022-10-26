0ce7b71cf3a73f2a23a05d539ef8f64b89dcdd88
Ziyuan(Jack) Jiang <48230839+ZiyuanJiang825@users.noreply.github.com>
2022-10-26 12:08:07 -0400

    Create user_log_infos table

---
+++ db/schema.rb
@@ -0,0 +1,21 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_10_26_155007) do
+
+  create_table "user_log_infos", primary_key: "username", id: :string, limit: 50, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.string "password", limit: 50
+    t.string "email", limit: 50
+    t.bigint "uid"
+  end
+
+end
