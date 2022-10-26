2cf74353076c96dc2ff00f69f142a55729ec73eb
Zixuan(Garrick) Zhang <garrickz2018@gmail.com>
2022-10-26 17:47:00 -0400

    add Logger helper

--- app/models/leet_logger.rb
+++ app/models/leet_logger.rb
@@ -1,5 +1,10 @@
 class LeetLogger
-    def LeetLogger.get_logger(file_path)
-        Logger.new("#{Rails.root}/log/#{file_path}")
+    def LeetLogger.get_logger(class_name='Default', file_path='other.log')
+        logger = Logger.new("#{Rails.root}/log/#{file_path}")
+        logger.datetime_format = 'yyyy-MM-dd HH:mm:ss,SSS'
+        logger.formatter = proc do |severity, datetime, progname, msg|
+            "[#{severity}] #{datetime} #{progname} [#{class_name}]- #{msg}\n"
+        end
+        logger
     end
 end
