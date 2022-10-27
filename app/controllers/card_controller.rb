fba0aee6b5fc97dbf0621bfc4190c2561cb13cff
zll <zll>
2022-10-27 01:16:01 -0400

    Add card_helper.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,8 +1,27 @@
 class CardController < ApplicationController
 
   def create
-    cid = Card.create_card params[:uid], params[:title], params[:source], params[:description], params[:schedule_time]
-    session[:cid] = cid
+    unless CardHelper.check_title_empty params[:title]
+      flash[:r_notice] = 'Title cannot be empty'
+      redirect_to
+      return
+    end
+    unless CardHelper.check_title_length params[:title]
+      flash[:r_notice] = 'The maximum length cannot exceed 50 characters'
+      redirect_to
+      return
+    end
+    unless CardHelper.check_source_length params[:source]
+      flash[:r_notice] = 'The maximum length cannot exceed 100 characters'
+      redirect_to
+      return
+    end
+    unless CardHelper.check_description_length params[:description]
+      flash[:r_notice] = 'The maximum length cannot exceed 300 characters'
+      redirect_to
+      return
+    end
+    cid = Card.create_card params[:uid], params[:title], params[:source], params[:description]
     redirect_to main_create_card_path
   end
 end
