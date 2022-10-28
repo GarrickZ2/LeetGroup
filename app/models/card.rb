1e5ddacd908f1e7fb224c7d4d7b8f58f5777d655
Zixuan(Garrick) Zhang <garrickz2018@gmail.com>
2022-10-27 23:57:01 -0400

    fix bug

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,7 @@
 class Card < ActiveRecord::Base
-  @@cid = 1
   def self.create_card(uid, title, source, description)
-    self.create(uid: uid, title: title, source: source, description: description, schedule_time: NULL, status: 0,
-                stars: 0, used_time: 0, create_time: Time.now, update_time: Time.now, cid: @@cid)
-    @@cid += 1
-    return @@cid
+    new_card = create(uid: uid, title: title, source: source, description: description, schedule_time: nil, status: 0,
+                      stars: 0, used_time: 0, create_time: Time.now, update_time: Time.now)
+    new_card.cid
   end
 end
