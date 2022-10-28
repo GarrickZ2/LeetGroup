413972c294bfa733a00588c7a4a3942437307724
zll <zll>
2022-10-28 12:16:44 -0400

    Modify card_spec and user_to_card model

--- app/models/user_to_card.rb
+++ app/models/user_to_card.rb
@@ -1,5 +1,3 @@
 class UserToCard < ActiveRecord::Base
-  def self.create_user_to_card(uid, cid)
-    create(uid: uid, cid: cid)
-  end
+
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 describe Card do
-  describe "test create card"  do
-    it "should insert a new record in card table successfully" do
-      cid = Card.create_card("1", "123", "123", "123")
-      expect(cid).to eq 1
+  describe 'test create card' do
+    it 'should insert a new record in card table successfully' do
+      cnt = Card.count
+      cid = Card.create_card('1', '123', '123', '123')
+      expect(Card.count).to eq cnt + 1
     end
   end
 end
