86e17c5d0a5e16692eabd5c323ab095893d44da5
Zixuan(Garrick) Zhang <garrickz2018@gmail.com>
2022-10-29 01:38:55 -0400

    stash

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,16 +4,20 @@
 # Used to redirect to the main page of the website
 class MainController < ApplicationController
   layout 'dashboard'
-
-  def dashboard
+  before_action :auth_check
+  def auth_check
     if session[:uid].nil?
       flash[:l_notice] = 'Pleases Login Account'
       redirect_to user_index_path type: 'login'
+      return
     end
-    @profile = UserHelper.get_profile session[:uid]
-    if @profile.nil?
+    session[:profile] = UserHelper.get_profile session[:uid] if session[:profile].nil?
+    if session[:profile].nil?
       flash[:l_notice] = 'User Information Invalid, Please Login Again'
       redirect_to user_index_path type: 'login'
     end
   end
+
+  def dashboard; end
+  def profile; end
 end
