bd43fe460d38c75239d87d1836806d6161311d0f
ruyuewang <rwang5@wpi.edu>
2022-11-03 21:45:04 -0400

    1) fix the profile page style 2) fix the create card style

--- public/user/profile.js
+++ public/user/profile.js
@@ -1,42 +1,3 @@
-function change_tab(name) {
-    $('#avatar-tab').removeClass('active')
-    // $('#view-tab').removeClass('active')
-    // $('#edit-tab').removeClass('active')
-
-    let avatar = $('#avatar')
-    // let view = $('#view')
-    // let edit = $('#edit')
-
-    avatar.removeClass('active')
-    // view.removeClass('active')
-    // edit.removeClass('active')
-
-    avatar.removeClass('fade')
-    // view.removeClass('fade')
-    // edit.removeClass('fade')
-
-    if (name === 'avatar') {
-        $('#avatar-tab').addClass('active')
-        avatar.addClass('active')
-        // view.addClass('fade')
-        // edit.addClass('fade')
-        $('#save-btn').text('Save Your Avatar')
-    }
-    // } else if (name === 'view') {
-    //     $('#view-tab').addClass('active')
-    //     view.addClass('active')
-    //     avatar.addClass('fade')
-    //     edit.addClass('fade')
-    //     $('#save-btn').text('View Your Profile')
-    // } else if (name === 'edit') {
-    //     $('#edit-tab').addClass('active')
-    //     edit.addClass('active')
-    //     view.addClass('fade')
-    //     avatar.addClass('fade')
-    //     $('#save-btn').text('Save Your Profile')
-    // }
-}
-
 function editProfile(name) {
     let role =  $("#inputRole");
     let school = $("#inputSchool");
@@ -73,22 +34,23 @@ function editProfile(name) {
         company.removeClass("edit-mode");
         city.removeClass("edit-mode");
         bio.removeClass("edit-mode");
+        location.reload();
     }
-
 }
 
 function showAvatarPanel() {
     $("#profile-edit-form").attr("hidden", true);
     $("#avatar-panel").removeAttr("hidden");
-    let title = $(".profile-header-title");
-    title.empty();
-    title.text("Change your avatar");
+    $(".profile-header-title-detail").attr("hidden", true);
+    $(".profile-header-title-avatar").removeAttr("hidden");
 
 }
 
 function hideAvatarPanel() {
     $("#avatar-panel").attr("hidden", true);
     $("#profile-edit-form").removeAttr("hidden");
+    $(".profile-header-title-detail").removeAttr("hidden");
+    $(".profile-header-title-avatar").attr("hidden", true);
 }
 
 function submitProfileChange() {
