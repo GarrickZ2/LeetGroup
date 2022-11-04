f1be84a291c1a3bcbc2f6a09d1320c31fce478b8
Ziyuan(Jack) Jiang <48230839+ZiyuanJiang825@users.noreply.github.com>
2022-11-03 23:11:51 -0400

    add user_to_groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_04_030101) do
+ActiveRecord::Schema.define(version: 2022_11_04_030754) do
 
   create_table "cards", primary_key: "cid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.bigint "uid"
@@ -60,6 +60,11 @@ ActiveRecord::Schema.define(version: 2022_11_04_030101) do
     t.bigint "uid"
     t.bigint "cid"
     t.index ["uid"], name: "index_user_to_cards_on_uid"
+  end
+
+  create_table "user_to_groups", primary_key: ["uid", "gid"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.bigint "uid", null: false
+    t.bigint "gid", null: false
   end
 
 end
