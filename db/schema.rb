d078a52bd35b8a09abc808f889b7ce31ec1d80a2
Zixuan(Garrick) Zhang <garrickz2018@gmail.com>
2022-11-04 12:44:19 -0400

    modify database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_31_214035) do
+ActiveRecord::Schema.define(version: 2022_11_04_041034) do
+
+  create_table "card_to_comments", primary_key: "comment_id", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "cid"
+    t.bigint "uid"
+    t.string "content", limit: 500
+    t.datetime "create_time"
+    t.index ["cid"], name: "index_card_to_comments_on_cid"
+  end
 
   create_table "cards", primary_key: "cid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "uid"
@@ -23,6 +31,25 @@ ActiveRecord::Schema.define(version: 2022_10_31_214035) do
     t.datetime "create_time"
     t.datetime "update_time"
     t.datetime "schedule_time"
+  end
+
+  create_table "group_infos", primary_key: "gid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 50
+    t.string "description", limit: 500
+    t.datetime "create_time"
+    t.integer "status", default: 0
+  end
+
+  create_table "group_to_cards", primary_key: ["gid", "cid"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "gid", null: false
+    t.bigint "cid", null: false
+  end
+
+  create_table "group_to_users", primary_key: ["gid", "uid"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "gid", null: false
+    t.bigint "uid", null: false
+    t.integer "role", default: 0
+    t.index ["uid"], name: "index_group_to_users_on_uid"
   end
 
   create_table "user_log_infos", primary_key: "username", id: :string, limit: 50, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
