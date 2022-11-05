c3b1996894238057e7d92c8d39743fa1bcd67b60
Zixuan(Garrick) Zhang <garrickz2018@gmail.com>
2022-11-05 16:46:09 -0400

    finish group user

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_11_05_144525) do
 
-  create_table "card_to_comments", primary_key: "comment_id", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "card_to_comments", primary_key: "comment_id", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "cid"
     t.bigint "uid"
     t.string "content", limit: 500
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2022_11_05_144525) do
     t.index ["cid"], name: "index_card_to_comments_on_cid"
   end
 
-  create_table "cards", primary_key: "cid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "cards", primary_key: "cid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "uid"
     t.string "title", limit: 80
     t.string "source", limit: 100
@@ -33,39 +33,39 @@ ActiveRecord::Schema.define(version: 2022_11_05_144525) do
     t.datetime "schedule_time"
   end
 
-  create_table "group_infos", primary_key: "gid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "group_infos", primary_key: "gid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", limit: 50
     t.string "description", limit: 500
     t.datetime "create_time"
     t.integer "status", default: 0
   end
 
-  create_table "group_to_cards", primary_key: ["gid", "cid"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "group_to_cards", primary_key: ["gid", "cid"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "gid", null: false
     t.bigint "cid", null: false
   end
 
-  create_table "group_to_users", primary_key: ["gid", "uid"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "group_to_users", primary_key: ["gid", "uid"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "gid", null: false
     t.bigint "uid", null: false
     t.integer "role", default: 0
     t.index ["uid"], name: "index_group_to_users_on_uid"
   end
 
-  create_table "group_welcome_codes", primary_key: "code", id: :string, limit: 10, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "group_welcome_codes", primary_key: "code", id: :string, limit: 10, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "gid"
     t.bigint "uid"
     t.integer "type", default: 0
     t.date "expiration_date"
   end
 
-  create_table "user_log_infos", primary_key: "username", id: :string, limit: 50, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "user_log_infos", primary_key: "username", id: :string, limit: 50, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "password", limit: 50
     t.string "email", limit: 50
     t.bigint "uid"
   end
 
-  create_table "user_profiles", primary_key: "uid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "user_profiles", primary_key: "uid", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username", limit: 50
     t.string "school", limit: 50
     t.string "company", limit: 50
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 2022_11_05_144525) do
     t.string "role", limit: 50
   end
 
-  create_table "user_to_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "user_to_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "uid"
     t.bigint "cid"
     t.index ["uid"], name: "index_user_to_cards_on_uid"
