c34a3f3b3f04f517b93d95b3d36cf5f94e531753
Ziyuan(Jack) Jiang <48230839+ZiyuanJiang825@users.noreply.github.com>
2022-11-05 20:15:56 -0400

    add/delete/get cards from groups

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -54,6 +54,20 @@ module GroupHelper
     GroupToUser.create(gid: gid, uid: uid, role: role)
   end
 
+  def self.add_card_to_group(cid, gid)
+    card = GroupToCard.find_by(gid: gid, cid: cid)
+    if card != nil
+      return
+    end
 
+    GroupToCard.create(gid: gid, cid: cid)
+  end
 
+  def self.delete_card_from_group(cid, gid)
+    GroupToCard.delete_by(gid: gid, cid: cid)
+  end
+
+  def self.get_card_from_group(gid)
+    GroupInfo.find_by(gid: gid)
+  end
 end
