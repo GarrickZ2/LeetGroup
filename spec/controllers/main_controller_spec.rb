de6c17760b8d787c8e5a4580e0b29193f25fb13f
MaggieZ211 <1091794274@qq.com>
2022-11-16 17:28:25 -0500

    add rspec test for main controller and card controller

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -14,4 +14,30 @@ describe MainController do
       expect(response).to render_template('main/dashboard')
     end
   end
+
+  describe 'User want to goto the group page' do
+    before(:each) do
+      GroupToUser.create(gid: 1, uid: 1)
+      UserProfile.delete_all
+      UserProfile.create(uid: 1, username: 'MaggieZ')
+      GroupInfo.delete_all
+      GroupInfo.create(gid: 1, name: 'ColumbiaStudent', description: 'Columbia students practicing group')
+    end
+
+    it "redirect to dashboard if user hasn't joined a group" do
+      session[:uid] = 2
+      get :group, params: {
+        gid: 1
+      }
+      expect(response).to redirect_to('/main/dashboard')
+    end
+
+    it 'should show all the groups the user joined' do
+      session[:uid] = 1
+      get :group, params: {
+        gid: 1
+      }
+      expect(response).to render_template('main/group')
+    end
+  end
 end
