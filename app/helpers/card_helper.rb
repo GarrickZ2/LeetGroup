4a3edbbe4b1c44a4cda2abe66767a62d87bd2242
Ziyuan(Jack) Jiang <48230839+ZiyuanJiang825@users.noreply.github.com>
2022-11-21 22:16:47 -0500

    add copy, change status and star help functions for card

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -50,4 +50,35 @@ module CardHelper
     end
     true
   end
+
+  def self.copy_card(copyFrom, copyTo, cid)
+    this_card = Card.find_by(uid: copyFrom, cid: cid)
+    return false if this_card.nil?
+    self.create_card(copyTo, this_card.title, this_card.source, this_card.description)
+    true
+  end
+
+  def self.change_status(uid, cid, status)
+    this_card = Card.find_by(uid: uid, cid: cid)
+    return false if this_card.nil?
+    this_card.status = status
+    this_card.save
+    true
+  end
+
+  def self.star(cid)
+    this_card = Card.find_by(cid: cid)
+    return false if this_card.nil?
+    this_card.stars += 1
+    this_card.save
+    true
+  end
+
+  def self.unstar(cid)
+    this_card = Card.find_by(cid: cid)
+    return false if this_card.nil?
+    this_card.stars -= 1
+    this_card.save
+    true
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,10 @@
 class Card < ActiveRecord::Base
+  @@card_status = { doing: 0, archived: 1, finished: 2}
+
+  def self.card_status
+    @@card_status
+  end
+
   def self.create_card(uid, title, source, description)
     new_card = create(uid: uid, title: title, source: source, description: description, schedule_time: nil, status: 0,
                       stars: 0, used_time: 0, create_time: Time.now, update_time: Time.now)
